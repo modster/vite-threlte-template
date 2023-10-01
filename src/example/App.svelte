245efe2fcf113e3768a0182fe03323312f4a679f
E. M. Greeff <emgreeff@datadash.cloud>
2023-10-01 09:45:40 -0600

    red, green, blue

--- src/example/App.svelte
+++ src/example/App.svelte
@@ -1,8 +1,33 @@
 <script>
   import { Canvas } from '@threlte/core'
   import Scene from './Scene.svelte'
+  	import RedThing from './RedThing.svelte';
+	import GreenThing from './GreenThing.svelte';
+	import BlueThing from './BlueThing.svelte';
+
+	const options = [
+		{ color: 'red', component: RedThing },
+		{ color: 'green', component: GreenThing },
+		{ color: 'blue', component: BlueThing }
+	];
+
+	let selected = options[2];
 </script>
+<div class="z">
+
+<select bind:value={selected}>
+	{#each options as option}
+		<option value={option}>{option.color}</option>
+	{/each}
+</select>
+
+</div>
 
 <Canvas>
-  <Scene />
+<svelte:component this={selected.component} />
 </Canvas>
+<style>
+  .z {
+    z-index: 10;
+  }
+</style>
